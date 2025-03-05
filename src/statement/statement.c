3961c755a41ede49b02b55573addab709f3a02a9
Max Müller <kontakt.maxmueller@outlook.com>
2025-03-05 10:47:21 +0100

    Added serialization and deserialization of rows

--- src/statement/statement.c
+++ src/statement/statement.c
@@ -5,6 +5,14 @@ PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement)
     if(strncmp(input_buffer->buffer, "insert", input_buffer->buffer_length) == 0)
     {
         statement->type = STATEMENT_INSERT;
+        int args_assigned = sscanf(
+          input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),
+          &(statement->row_to_insert.username), &(statement->row_to_insert.email)
+        );
+        if (args_assigned < 3)
+        {
+          return PREPARE_SYNTAX_ERROR;
+        }
         return PREPARE_SUCCESS;
     }
     else if(strncmp(input_buffer->buffer, "select", input_buffer->buffer_length) == 0) 
@@ -27,3 +35,17 @@ void execute_statement(Statement* statement) {
     }
   }
   
+
+void serialize_row(Row* source, void* destination)
+{
+  memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+  memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+  memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+
+void deserialize_row(void* source, Row* destination)
+{
+  memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+  memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+  memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+}
--- src/statement/statement.h
+++ src/statement/statement.h
@@ -3,7 +3,28 @@
 
 #include <string.h>
 #include <stdio.h>
+#include <stdint.h>
 #include "../input_buffer/input_buffer.h"
+
+#define USERNAME_MAX_SIZE 32
+#define EMAIL_MAX_SIZE 255
+
+#define size_of_attribute(Struct, Attriute) sizeof(((Struct*)0)->Attriute)
+
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+
+typedef struct {
+    uint32_t id,
+    char username[USERNAME_MAX_SIZE],
+    char email[EMAIL_MAX_SIZE]
+} Row;
 
 typedef enum {
     STATEMENT_INSERT,
@@ -17,9 +38,13 @@ typedef enum {
 
 typedef struct {
     StatementType type;
+    Row row_to_insert;
 } Statement;
 
 PrepareResult prepare_statement(InputBuffer*, Statement*);
 void execute_statement(Statement*);
+
+void serialize_row(Row* source, void* destination);
+void deserialize_row(void* source, Row* destination);
 
 #endif
