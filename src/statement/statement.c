6d8c6348827df3464369727a0887453552470e82
Max Müller <kontakt.maxmueller@outlook.com>
2025-03-05 10:52:04 +0100

    Added syntax error to prepareresult and fixed row not found

--- src/statement/statement.c
+++ src/statement/statement.c
@@ -1,5 +1,13 @@
 #include "statement.h"
 
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement)
 {
     if(strncmp(input_buffer->buffer, "insert", input_buffer->buffer_length) == 0)
--- src/statement/statement.h
+++ src/statement/statement.h
@@ -11,19 +11,10 @@
 
 #define size_of_attribute(Struct, Attriute) sizeof(((Struct*)0)->Attriute)
 
-const uint32_t ID_SIZE = size_of_attribute(Row, id);
-const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
-const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
-const uint32_t ID_OFFSET = 0;
-const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
-const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
-const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
-
-
 typedef struct {
-    uint32_t id,
-    char username[USERNAME_MAX_SIZE],
-    char email[EMAIL_MAX_SIZE]
+    uint32_t id;
+    char username[USERNAME_MAX_SIZE];
+    char email[EMAIL_MAX_SIZE];
 } Row;
 
 typedef enum {
@@ -33,6 +24,7 @@ typedef enum {
 
 typedef enum {
     PREPARE_SUCCESS,
+    PREPARE_SYNTAX_ERROR,
     PREPARE_UNRECOGNIZED_STATEMENT
 } PrepareResult;
 
